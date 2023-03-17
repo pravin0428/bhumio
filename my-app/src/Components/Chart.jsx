f18a93c817936487121a6c8738a3734681640932
pravin0428 <mohite46199@gmail.com>
2023-03-17 13:47:44 +0530

    added the improvement note @chart.js component

--- my-app/src/Components/Chart.jsx
+++ my-app/src/Components/Chart.jsx
@@ -62,6 +62,8 @@ function Chart() {
     setMiscFees(value);
   };
 
+  //note : need some improment to make the slider 
+  //as inbuild in graph and i have done it at ouside 
   return (
     <>
       <Container maxW={"4xl"} p="12" display="flex" gap={4} mt={10}>
